283d16fdb6e36a2c9e496b7b32fcb3a2492893da
Nes-Martinez <nes.martinez@tuta.io>
2021-02-13 19:25:46 -0500

    Added: Smallest Difference

---
+++ AlgoExpert/Medium/08-Smallest-Difference.js
@@ -0,0 +1,34 @@
+ 
+ // 0(nlog(n) +  m(log(m)) time | 0(1) space
+ 
+function smallestDifference(arrayOne, arrayTwo) {
+    arrayOne.sort((a, b) => a - b);
+    arrayTwo.sort((a, b) => a - b);
+    let smallest = Infinity;
+    let current = Infinity;
+    let smallestPair = [];
+    let indexOne = 0;
+    let indexTwo = 0;
+
+    while (indexOne < arrayOne.length && indexTwo < arrayTwo.length) {
+      let first= arrayOne[indexOne];
+      let second = arrayTwo[indexTwo];
+
+      if (first < second) {
+        current = second - first;
+        indexOne++;
+      } else if (second < first) {
+        current = first - second;
+        indexTwo++;
+      } else {
+        return [first, second];
+      }
+
+      if (smallest > current) {
+        smallest = current;
+        smallestPair = [first, second];
+      }
+    }
+
+    return smallestPair;
+  }
